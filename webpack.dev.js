7871ee43e31e9c2d47261ee2fcdf5af9924c8ee6
Cauan <cauandeoliveiracosta@gmail.com>
2024-06-11 20:01:41 -0300

    feat: add initial HTML structure

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,9 +2,10 @@ const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  mode: "development",
-  devtool: "inline-source-map",
-  devServer: {
-    static: "./dist",
-  },
+    mode: "development",
+    devtool: "inline-source-map",
+    devServer: {
+        static: "./dist",
+        watchFiles: ["./src/*.html"],
+    },
 });
